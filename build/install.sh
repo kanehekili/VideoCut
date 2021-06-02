3c23521c6f3561d2d4a08fc33eed040522d96b80
kanehekili <kanehekili@msi>
2021-06-03 00:24:12 +0200

    changed installation to opt/videocut

--- build/install.sh
+++ build/install.sh
@@ -7,9 +7,9 @@ fi
 #copy desktop to /usr/share applications
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 sudo cp $DIR/VideoCut.desktop /usr/share/applications;
-sudo mkdir -p /usr/local/bin/videocut;
-sudo cp -r $DIR/* /usr/local/bin/videocut/;
-sudo ln -s /usr/local/bin/videocut/VideoCut.py /usr/local/bin/VideoCut
+sudo mkdir -p /opt/videocut;
+sudo cp -r $DIR/* /opt/videocut/;
+sudo ln -s /opt/videocut/VideoCut.py /usr/bin/VideoCut
 
 echo "######################################################################"
 echo "#                  Ensure you have installed:                        #"                     
